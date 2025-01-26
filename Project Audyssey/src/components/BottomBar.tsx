083c10673a4b72371280e8e1992931a143c1db1e
Kathir Aroun <kathir.aroun@outlook.com>
2025-01-26 19:08:30 +0000

    Created folders to store React Components and component-specific stylesheets

---
+++ Project Audyssey/src/components/BottomBar.tsx
@@ -0,0 +1,13 @@
+import "./BottomBar.css";
+
+function BottomBar() {
+
+
+    return(
+        <div className="outline">
+            <p>This is the bottom bar.</p>
+        </div>
+    );
+}
+
+export default BottomBar;
