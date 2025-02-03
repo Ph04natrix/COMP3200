fcfa9546782d4350257ef83f94b6c0de9ac8a4ed
Kathir Aroun <kathir.aroun@outlook.com>
2025-02-03 01:11:32 +0000

    converted from store to app state to manage state

--- Project Audyssey/src-tauri/src/lib.rs
+++ Project Audyssey/src-tauri/src/lib.rs
@@ -1,14 +1,17 @@
 use std::sync::Mutex;
-use serde_json::json;
+use serde::Deserialize;
 use spotify::LoginState;
 use tauri::{Builder, Manager};
-use tauri_plugin_store::*;
 
-struct AppStateInner {
+mod spotify;
+
+pub struct AppStateInner {
   ClientID: String,
   ClientSecret: String,
+  Redirect: String,
   Login: LoginState,
-  AccessToken: String,
+  AccessToken: AccessToken,
+  CodeVerifier: String,
 }
 
 impl AppStateInner {
@@ -16,39 +19,36 @@ impl AppStateInner {
         AppStateInner {
             ClientID: "71362bad121c4dd5be0fd0794119454b".to_string(),
             ClientSecret: "f8f9676547104ee080c3b61c1276b9c6".to_string(),
+            Redirect: String::from("http://localhost:1420"),
             Login: LoginState::LoggedOut,
-            AccessToken: "".to_string()
+            AccessToken: AccessToken::default(),
+            CodeVerifier: "".to_string()
         }
     }
 }
 
-type AppState = Mutex<AppStateInner>;
+pub type AppState = Mutex<AppStateInner>;
 
-mod spotify;
+#[derive(Deserialize, Default)]
+struct AccessToken {
+    access_token: String,
+    token_type: String,
+    scope: String,
+    expires_in: u32,
+    refresh_token: String
+}
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     Builder::default()
         .setup(|app| {// Sets up the state for the application
-            //app.manage(Mutex::new(AppStateInner::default()));
-
-            let store = app.store("store.json")?;
-
-            // Note that values must be serde_json::Value instances,
-            // otherwise, they will not be compatible with the JavaScript bindings.
-            store.set("client-id", json!({ "value": "71362bad121c4dd5be0fd0794119454b" }));
-            store.set("client-secret", json!({ "value": "f8f9676547104ee080c3b61c1276b9c6"}));
-            // store.set("access-token", json!({"value": ""}));
-
-            // Get a value from the store.
-            let value = store.get("client-id").expect("Failed to get client-id from store");
-            println!("{}", value); // {"value":5}
+            app.manage(Mutex::new(AppStateInner::default()));
 
             Ok(())
         })
         .plugin(tauri_plugin_opener::init())
         .invoke_handler(tauri::generate_handler![
-            spotify::make_request,
+            spotify::make_request, //spotify::request_access_code, spotify::start_login,
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
--- Project Audyssey/src/components/Login.tsx
+++ Project Audyssey/src/components/Login.tsx
@@ -1,8 +1,14 @@
+import { invoke } from "@tauri-apps/api/core";
+
 function Login() {
-    
+
     return(
         <div className="container">
-            
+            <button type="button" onClick={() => invoke("start_login()")}>
+                Grant access to your library
+            </button>
         </div>
     )
 }
+
+export default Login;
