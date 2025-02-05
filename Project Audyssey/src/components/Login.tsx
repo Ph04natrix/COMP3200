9986b5b5713eb84e79d88d49180613c512503d35
Kathir Aroun <kathir.aroun@outlook.com>
2025-02-05 14:03:51 +0000

    Login page now redirects to Spotify OAuth and back

--- Project Audyssey/src/components/Login.tsx
+++ Project Audyssey/src/components/Login.tsx
@@ -1,13 +1,23 @@
 import { invoke } from "@tauri-apps/api/core";
 
 function Login() {
+    function handleClick() {
+        console.log("Grant access to the Audyssey button clicked");
+        invoke<string>("start_login")
+            .then((url) => window.location.replace(url))
+            .catch((err) => console.error(err));
+    }
 
     return(
-        <div className="container">
-            <button type="button" onClick={() => invoke("start_login()")}>
+        <>
+            <p>
+                To use this application, we require access to your Spotify Library.
+                This access can be granted by pressing the below button:               
+            </p>
+            <button type="button" onClick={handleClick}>
                 Grant access to your library
             </button>
-        </div>
+        </>
     )
 }
 
