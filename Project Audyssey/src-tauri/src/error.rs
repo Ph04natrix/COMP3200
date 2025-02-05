922e4ecf543099d0ca9fbb0b1cc4d5352e011374
Kathir Aroun <kathir.aroun@outlook.com>
2025-02-05 14:01:57 +0000

    Added reqwest and url error types to MyError

--- Project Audyssey/src-tauri/src/error.rs
+++ Project Audyssey/src-tauri/src/error.rs
@@ -1,12 +1,22 @@
+use std::fmt::{self, write};
+
+use tauri_plugin_http::reqwest;
+
+pub type MyResult<T> = std::result::Result<T, MyError>;
+
 #[derive(Debug, thiserror::Error)]
 pub enum MyError {
   #[error(transparent)]
-  Io(#[from] std::io::Error)
+  Io(#[from] std::io::Error),
+  #[error("Failed to handle reqwest error")]
+  Reqwest(#[from] reqwest::Error),
+  #[error("Failed to handle ParseError")]
+  URLParse(#[from] url::ParseError)
 }
 
 // we must manually implement serde::Serialize
 impl serde::Serialize for MyError {
-  fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+  fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
   where
     S: serde::ser::Serializer,
   {
