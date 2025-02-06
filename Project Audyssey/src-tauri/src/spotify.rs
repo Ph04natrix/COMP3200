55a8f4661a1dccb9c29c0c703703e2884c733924
Kathir Aroun <kathir.aroun@outlook.com>
2025-02-06 15:13:16 +0000

    cleaned up spotify.rs code and modified variable names

--- Project Audyssey/src-tauri/src/spotify.rs
+++ Project Audyssey/src-tauri/src/spotify.rs
@@ -1,4 +1,4 @@
-use tauri::{State, Url, Window};
+use tauri::{State, Url};
 use tauri_plugin_http::reqwest::*;
 
 use crate::{
@@ -13,18 +13,38 @@ pub enum LoginState {
     LoggedIn
 }
 
-/* // TODO implement the PKCE authorisation flow to allow a user to be authenticated
+pub enum SpotifyEndpoints {
+    SavedTracks,
+}
 
-add an intermediary loading screen between the login and main home page, if its the first time the user is granting access
+const SPOTIFY_API: &str = "https://api.spotify.com/v1/";
 
-then change the window from the login screen to the main home page
-
-instead of putting things in the localStorage, put it in the store.json (do this in login.tsx)
-
+/*
+todo Should this return an object of type RequestBuilder? Goes from being async to more pure and simpler
+todo means that the access_token needs to be passed into the function.
 */
-
 #[tauri::command]
-pub async fn make_request(){
+pub async fn build_request(
+    endpoint: SpotifyEndpoints,
+    state: State<'_, AppState>
+) -> MyResult<String>{
+    let specific_endpoint = match (endpoint) {
+        SpotifyEndpoints::SavedTracks => "me/tracks"
+    };
+
+    let mut url: String = SPOTIFY_API.to_string();
+    url += specific_endpoint;
+
+    let state_lock = state.lock().await;
+    let token = &state_lock.AccessToken.access_token;
+
+    let method = Method::GET;
+
+    let client = Client::new();
+    client.request(method, url).bearer_auth(token);
+
+    Ok("Request built".to_string())
+
   
 }
 
@@ -52,43 +72,42 @@ pub async fn start_login(
         ("code_challenge", &codeChallenge),
     ];
 
-    let authEndpoint = "https://accounts.spotify.com/authorize".to_string();
-    let auth_url = Url::parse_with_params(&authEndpoint, &params);
+    let auth_endpoint = "https://accounts.spotify.com/authorize".to_string();
+    let auth_url = Url::parse_with_params(&auth_endpoint, &params);
 
     match auth_url {
         Ok(url) => Ok(url.to_string()),
         Err(err) => Err(MyError::URLParse(err))
     }
+
+    // todo instead of changing the url of this window, open a new window so that the result can 
     
 }
 
 #[tauri::command]
-pub async fn request_access_code(
-    authResponse: Result<String>,
+pub async fn request_access_token(
+    auth_code: String,
     state: State<'_, AppState>
-) -> Result<()> {
-    let code: String = match authResponse.into() {
-        Ok(code) => code,
-        Err(e) => return Err(e),
-    };
-
+) -> MyResult<String> {
     let mut state_lock = state.lock().await;
     let redirectURI = &state_lock.Redirect;
     let clientID = &state_lock.ClientID;
-    let codeVerifier = &state_lock.CodeVerifier;
-
-    let client = Client::new();
+    let code_verifier = &state_lock.CodeVerifier;
+    
     let params = [
         ("grant_type", "authorization_code"),
-        ("code", &code),
+        ("code", &auth_code),
         ("redirect_uri", redirectURI),
         ("client_id", clientID),
-        ("code_verifier", codeVerifier)
+        ("code_verifier", code_verifier)
     ];
 
-    let tokenEndpoint = "https://accounts.spotify.com/api/token".to_string();
-    println!("Parameterised Token Endpoint: {tokenEndpoint}");
-    let url = Url::parse_with_params(&tokenEndpoint, &params);
+    let token_endpoint = "https://accounts.spotify.com/api/token".to_string();
+    println!("Parameterised Token Endpoint: {token_endpoint}");
+    
+    let url = Url::parse_with_params(&token_endpoint, &params);
+    
+    let client = Client::new();
     let res = client
         .post(url.expect("Failed to send POST request"))
         .header("Content-Type", "application/x-ww-form-urlencoded")
@@ -97,9 +116,10 @@ pub async fn request_access_code(
 
     let body = res.unwrap().json::<AccessToken>().await;
     state_lock.AccessToken = body.expect("Failed to get content from access token request response");
+    
     print!("Access token successfully acquired");
     
-    Ok(())
+    Ok(String::from("Thank you for granting access! Welcome to the Audyssey"))
 }
 
 // Generates both a code verifier and a code challenge
