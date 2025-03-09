6898ba15d158401ffc1f76490148860bb07d2c23
Kathir Aroun <kathir.aroun@outlook.com>
2025-03-09 23:16:45 +0000

    songs without attributes are queried, requests sent sync using ureq and component operations are deferred

--- Project Audyssey/src-tauri/src/api/soundcharts.rs
+++ Project Audyssey/src-tauri/src/api/soundcharts.rs
@@ -1,64 +1,132 @@
+use flecs_ecs::prelude::{Builder, QueryAPI, QueryBuilderImpl};
 use serde::{Serialize, Deserialize};
-use tauri::http::{HeaderMap, StatusCode};
-use tauri_plugin_http::reqwest::Client;
+use tauri::{http::StatusCode, Emitter};
 
-use crate::error::MyResult;
+use crate::{ecs::types::{Acousticness, Danceability, Energy, Genres, Instrumentalness, Key, Liveness, Loudness, MissingAttributes, Mode, Name, Song, Speechiness, SpotifyID, Tempo, TimeSignature, Valence}, error::MyResult, AppState};
 
-use super::conversion::{Attributes, MinimalTrackObject};
+use super::conversion::Attributes;
 
-pub async fn get_song_audio_attributes(
-    songs: Vec<MinimalTrackObject>,
-    mut limit: u16
-) -> MyResult<Vec<MinimalTrackObject>>{
-    let client = Client::new();
-    let base_endpoint = "https://customer.api.soundcharts.com/api/v2.25/song/by-platform/spotify";
+#[tauri::command]
+pub async fn song_without_attributes_count(
+    state: tauri::State<'_, AppState>
+) -> MyResult<u32> {
+    let state_lock = state.lock().await;
+    let world = &state_lock.ecs_world;
 
-    let mut attributed_songs: Vec<MinimalTrackObject> = Vec::new();
-    
-    for song in songs {
-        if limit > 0 {
-            let param = &song.spotify_id;
-            let url = format!("{base_endpoint}/{param}");
-            let mut updated_song: MinimalTrackObject = song;
+    let q = world
+        .query::<&Name>()
+        .with::<&Song>()
+        .with::<&MissingAttributes>()
+        .build()
+    ;
 
-            if let Ok(res) = client
-                .get(url)
-                .header("x-app-id", "UNIVERSITY-OF-SOUTHAMPTON_59CFD081")
-                .header("x-api-key", "66017e280fb05d6b")
-                .send().await {
-                    limit -= 1;
-                    match res.status() {
-                        StatusCode::OK => {
-                            let song_attributes: Attributes = res.json::<SoundChartsResponse>().await?.object.into();
-                            updated_song.attributes = Some(song_attributes);
-                            println!("Received attributes for {}, limit={limit}", updated_song.name);
-                        },
-                        StatusCode::BAD_REQUEST | StatusCode::UNAUTHORIZED | StatusCode::FORBIDDEN | StatusCode::NOT_FOUND | StatusCode::GONE=> {
-                            let err_res = res.json::<SoundChartsErrorResponse>().await?;
-                            for err in err_res.errors {
-                                eprintln!("[{}] {}", err.code, err.message);
-                            }
-                        },
-                        _ => panic!("Unrecognised status code received from SoundCharts Request")
-                    }
-                }
-            attributed_songs.push(updated_song);
-        }
-        else {
-            println!("Limit={limit}");
-            break
-        }
-    }
+    let mut count = 0;
+    println!("Counting songs without attributes");
+    q.each(|n| {
+        println!("Song {} is missing attributes", n.0);
+        count += 1;
+    });
 
-    Ok(attributed_songs)
+    Ok(count)
 }
 
 #[tauri::command]
-pub async fn update_song_attributes(
+pub async fn fill_song_attributes(
+    state: tauri::State<'_, AppState>,
+    app: tauri::AppHandle
+) -> MyResult<String> {
+    let state_lock = state.lock().await;
+    let world = &state_lock.ecs_world;
 
-) -> MyResult<()> {
+    let q = world
+        .query::<(&SpotifyID, &Name)>()
+        .with::<&Song>()
+        .with::<MissingAttributes>()
+        .build()
+    ;
+
+    let mut limit = 2;
+    let base_endpoint = "https://customer.api.soundcharts.com/api/v2.25/song/by-platform/spotify";
     
-    Ok(())
+    world.defer(|| {
+        q.run_iter(|it, (s_id, name)| {
+            // dbg!(it.archetype());
+            dbg!(it.count());
+            
+            for i in it.iter() {
+                let song_ent = it.entity(i);
+
+                if limit > 0 {
+                    let url = format!("{base_endpoint}/{}", s_id[i].0);
+                   
+                    if let Ok(res) = ureq::get(url)
+                        .header("x-app-id", "UNIVERSITY-OF-SOUTHAMPTON_59CFD081")
+                        .header("x-api-key", "66017e280fb05d6b")
+                        .call() {
+                            limit -= 1;
+                            match res.status() {
+                                StatusCode::OK => {
+                                    let attrs: Attributes = res
+                                        .into_body()
+                                        .read_json::<SoundChartsResponse>()
+                                        .expect("Couldn't deserialize response into SoundChartsResponse")
+                                        .object.into()
+                                    ;
+                                    
+                                    // dbg!(world.is_deferred());
+                                    song_ent
+                                    .set(Acousticness(attrs.acousticness))
+                                    .set(Danceability(attrs.danceability))
+                                    .set(Energy(attrs.energy))
+                                    .set(Valence(attrs.valence))
+                                    .set(Tempo(attrs.tempo))
+                                    .set(Speechiness(attrs.speechiness))
+                                    .set(Liveness(attrs.liveness))
+                                    .set(Loudness(attrs.loudness))
+                                    .set(Instrumentalness(attrs.instrumentalness))
+                                    .set(Mode::try_from(attrs.mode).expect("Mode is not 0 or 1"))
+                                    .set(TimeSignature(attrs.time_signature))
+                                    .set(Key::try_from(attrs.key).expect("Key is not in range 3..7"))
+                                    .set(Genres(attrs.genres))
+                                    .remove::<MissingAttributes>()
+                                    ;
+                                    // dbg!(song_ent.has::<MissingAttributes>());
+                                    println!("Received attributes for {}, limit={limit}", name[i].0);
+        
+                                    app.emit("soundcharts-update-progress", SoundChartsUpdateProgress {
+                                        updated_song: name[i].0.clone(),
+                                    }).expect("Couldn't emit event: soundcharts-update-progress");
+                                    },
+                                StatusCode::BAD_REQUEST | StatusCode::UNAUTHORIZED | StatusCode::FORBIDDEN | StatusCode::NOT_FOUND | StatusCode::GONE=> {
+                                    let err_res = res
+                                        .into_body()
+                                        .read_json::<SoundChartsErrorResponse>()
+                                        .expect("Couldn't deserialize response into SoundChartsErrorResponse")
+                                    ;
+                                    for err in err_res.errors {
+                                        eprintln!("[{}] {}", err.code, err.message);
+                                    }
+                                },
+                                _ => panic!("Unrecognised status code received from SoundCharts Request")
+                            }
+                    } else {
+                        eprintln!("Could not get attributes from SoundCharts")
+                    }
+                } else { println!("Limit={limit}"); } // limit should be 0
+            }
+        }
+    )});
+    // dbg!(world.is_deferred());
+
+    app.emit("soundcharts-update-finished", 0).unwrap();
+
+    Ok("command fill_song_attributes finished".to_string())
+}
+
+#[derive(Clone, Serialize)]
+#[serde(rename_all = "camelCase")]
+struct SoundChartsUpdateProgress {
+    updated_song: String,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
