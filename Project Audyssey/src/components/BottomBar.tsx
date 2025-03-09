dd4e0548a07bcd019582563a936f05a8124f1549
Kathir Aroun <kathir.aroun@outlook.com>
2025-03-09 23:14:58 +0000

    frontend UI framework setup

--- Project Audyssey/src/components/BottomBar.tsx
+++ Project Audyssey/src/components/BottomBar.tsx
@@ -5,6 +5,11 @@ function BottomBar() {
 
     return(
         <div className="outline">
+            <div className="ActiveSong"></div>
+            <div className="PlaybackControl"></div>
+            <div className="SettingsAndFaff">{/* List of buttons, including the settings button*/}
+                <p></p>
+            </div>
             <p>This is the bottom bar.</p>
         </div>
     );
--- Project Audyssey/src/components/Main.tsx
+++ Project Audyssey/src/components/Main.tsx
@@ -1,5 +1,43 @@
+import { useState } from "react";
+import BottomBar from "./BottomBar";
+
+/* //todo
+Can use source which is just one audio collection
+Or can use multiple sources of track
+
+*/
+type SongCollection = {
+    type: SongCollectionType,
+    name: string
+}
+
+type SongCollectionType
+ = "Library"
+ | "Album" | "Compilation" | "Single"
+ | "Playlist"
+;
+
 export default function Main() {
-    return(
-        <p>This is where the main page of the application will be.</p>
-    );
+    const [currSongCollection, setCurrSongCollection] = useState<SongCollection>({
+        type: "Library",
+        name: ""
+    });
+       
+    return(<>
+    <div className="TopSection">
+        <div className="title">
+            <h1>
+                {currSongCollection.type === "Library" ? "Your Library" : (currSongCollection.type + ": " + currSongCollection.name)}
+            </h1>
+        </div>
+    </div>
+    <div className="flex-row">
+        <div className="LeftColumn"></div>
+        <div className="CenterColumn">
+            <p>This is where the main page of the application will be.</p>
+        </div>
+        <div className="RightColumn"></div>
+    </div>
+    <BottomBar />
+    </>);
 }
