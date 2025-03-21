f839b198071e97a660a251758a10b6ccea27c1b0
Kathir Aroun <kathir.aroun@outlook.com>
2025-03-21 16:15:38 +0000

    fixed main ts types, added string literals for easy coercion to string

--- Project Audyssey/src/types/audioResources.ts
+++ Project Audyssey/src/types/audioResources.ts
@@ -1,42 +1,30 @@
-export type Axis = {metric: string, orient: SpatialDimension}
-
-export enum SpatialDimension { X, Y, Z }
-export type Metric = ContinuousMetric | DiscreteMetric
-
-export enum ContinuousMetric {
-    // value between 0.0 and 1.0
-    Acousticness,
-    Danceability,
-    Energy,
-    Valence,
-    Speechiness,
-    Liveliness,
-    Loudness,
-    Instrumentalness,
-    // value between 0.0 and 120.0
-    Tempo
-}
-
-export enum DiscreteMetric {
-    Mode,
-    Explicit,
-    TimeSignature,
-    Key,
-    Genres
-}
-
-export type SongColView
- = { type: "Dashboard" }
- | { type: "Table" }
- | { type: "StaticGraph", axes: [Axis]}
- | { type: "DynamicGraph" }
-;
+/* //todo
+Can use source which is just one audio collection
+Or can use multiple sources of track
+*/
+export type AudioResource = Song | SongCollection;
 
 export type Song = {
     type: "Song"
     name: string,
     duration: number,
+    acousticness: number,
+    danceability: number,
+    energy: number,
+    valence: number,
+    tempo: number,
+    speechiness: number,
+    liveness: number,
+    loudness: number,
+    instrumentalness: number,
+    coords: {
+        x: number,
+        y: number,
+        z: number
+    }
 }
+
+export type LowercaseAttr = keyof Omit<Song, "type" | "name" | "duration" | "coords">;
 
 export type SongCollection = {
     type: SongColType,
@@ -52,4 +40,48 @@ export enum SongColType {
     Playlist
 }
 
-export type AudioResource = Song | SongCollection;
+export type SongColView
+    = "Dashboard"
+    | "Table"
+    | "StaticGraph"
+    | "DynamicGraph"
+    ;
+
+export enum SpatialDimension { X="X", Y="Y", Z="Z" }
+export type Metric = ContinuousMetric | DiscreteMetric
+
+export enum ContinuousMetric {
+    // value between 0.0 and 1.0
+    Acousticness="Acousticness",
+    Danceability="Danceability",
+    Energy="Energy",
+    Valence="Valence",
+    Speechiness="Speechiness",
+    Liveness="Liveness",
+    Loudness="Loudness",
+    Instrumental="Instrumental",
+    // value between 0.0 and 120.0
+    Tempo="Tempo",
+    // value between 0 and infinity?
+    // todo Duration="Duration"
+}
+
+export enum DiscreteMetric {
+    Mode,
+    Explicit,
+    TimeSignature,
+    Key,
+    Genres
+}
+
+export type AttrSelect = {
+    attr: ContinuousMetric,
+    use: "Unused" | SpatialDimension,
+    active: boolean
+    min: 0,
+    range: {
+        currMin: number,
+        currMax: number,
+    },
+    max: 1 | 120,
+}
