8167bf13132ff022a727f4f237f84d3275fb7ef8
Kathir Aroun <kathir.aroun@outlook.com>
2025-03-24 14:25:32 +0000

    song context menu framework added

---
+++ Project Audyssey/src/components/Home/CenterColumn/SongContextMenu.tsx
@@ -0,0 +1,13 @@
+import { Song } from "../../../types/audioResources"
+
+export default function SongContextMenu(props: {
+    selectedSong: Song
+}) {
+    // has a list of options
+
+
+    return(<>
+    {/* For each thing in the list return a button that does something */}    
+    </>)
+
+}
